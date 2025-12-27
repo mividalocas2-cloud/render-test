cf2a4c558975c1602fbaac36badc2d87516ac4d9
toshiya <mi.vida.loca.s2@gmail.com>
2025-12-27 09:35:23 +0900

    フォーム動作チェック用に修正

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
-def top():
-    return "<h1>休暇申請テスト（Render）</h1>"
+def form(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+
+@app.post("/confirm", response_class=HTMLResponse)
+def confirm(
+    request: Request,
+    department: str = Form(...),
+    name: str = Form(...),
+    start: str = Form(...),
+    end: str = Form(...),
+    days: str = Form(...),
+    reason: str = Form(...),
+    other_reason: str = Form("")
+):
+    return templates.TemplateResponse(
+        "confirm.html",
+        {
+            "request": request,
+            "department": department,
+            "name": name,
+            "start": start,
+            "end": end,
+            "days": days,
+            "reason": other_reason if reason == "other" else reason,
+        }
+    )
+
+
+@app.post("/complete", response_class=HTMLResponse)
+def complete(request: Request):
+    return templates.TemplateResponse("complete.html", {"request": request})
