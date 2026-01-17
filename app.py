aa2b9bbfd8fa053458f287bb377a517198a3980d
mividalocas2-cloud <mi.vida.loca.s2@gmail.com>
2026-01-17 10:27:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,7 @@
-import os
-import time
-import jwt
-import requests
+from fastapi import FastAPI
 
-CLIENT_ID = os.environ["LINEWORKS_CLIENT_ID"]
-CLIENT_SECRET = os.environ["LINEWORKS_CLIENT_SECRET"]
+app = FastAPI()
 
-TOKEN_URL = "https://auth.worksmobile.com/oauth2/v2.0/token"
-
-now = int(time.time())
-
-payload = {
-    "iss": CLIENT_ID,
-    "sub": CLIENT_ID,
-    "aud": TOKEN_URL,
-    "iat": now,
-    "exp": now + 3600
-}
-
-assertion = jwt.encode(
-    payload,
-    CLIENT_SECRET,
-    algorithm="HS256"
-)
-
-data = {
-    "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
-    "assertion": assertion
-}
-
-res = requests.post(TOKEN_URL, data=data)
-
-print(res.status_code)
-print(res.text)
+@app.get("/")
+def health():
+    return {"status": "ok"}
