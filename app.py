a07dc227e87b61e7e91f66e52aba01180b1153b2
mividalocas2-cloud <mi.vida.loca.s2@gmail.com>
2026-01-17 11:17:53 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import os
 import time
 import json
+import base64
 import jwt
 import requests
-from fastapi import FastAPI
-
-app = FastAPI()
 
 TOKEN_URL = "https://auth.worksmobile.com/oauth2/v2.0/token"
 
 def get_access_token():
     client_id = os.environ["LINEWORKS_CLIENT_ID"]
-    private_key_json = os.environ["LINEWORKS_PRIVATE_KEY"]
 
-    key_data = json.loads(private_key_json)
+    key_b64 = os.environ["LINEWORKS_PRIVATE_KEY_B64"]
+    key_json = base64.b64decode(key_b64).decode("utf-8")
+    key_data = json.loads(key_json)
 
     now = int(time.time())
 
@@ -41,9 +40,3 @@ def get_access_token():
     res.raise_for_status()
 
     return res.json()["access_token"]
-
-
-@app.get("/token-test")
-def token_test():
-    token = get_access_token()
-    return {"access_token": token[:30] + "..."}
