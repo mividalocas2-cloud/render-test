d75478ba1a7fdc0bd9263c382556f2eb3fe69e3b
mividalocas2-cloud <mi.vida.loca.s2@gmail.com>
2026-01-17 10:22:01 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 import os
+import time
+import jwt
 import requests
 
-CLIENT_ID = os.environ.get("LINEWORKS_CLIENT_ID")
-CLIENT_SECRET = os.environ.get("LINEWORKS_CLIENT_SECRET")
+CLIENT_ID = os.environ["LINEWORKS_CLIENT_ID"]
+CLIENT_SECRET = os.environ["LINEWORKS_CLIENT_SECRET"]
 
 TOKEN_URL = "https://auth.worksmobile.com/oauth2/v2.0/token"
 
+now = int(time.time())
+
+payload = {
+    "iss": CLIENT_ID,
+    "sub": CLIENT_ID,
+    "aud": TOKEN_URL,
+    "iat": now,
+    "exp": now + 3600
+}
+
+assertion = jwt.encode(
+    payload,
+    CLIENT_SECRET,
+    algorithm="HS256"
+)
+
 data = {
-    "grant_type": "client_credentials",
-    "client_id": CLIENT_ID,
-    "client_secret": CLIENT_SECRET,
-    "scope": "bot"
+    "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+    "assertion": assertion
 }
 
 res = requests.post(TOKEN_URL, data=data)
 
-print("status:", res.status_code)
+print(res.status_code)
 print(res.text)
