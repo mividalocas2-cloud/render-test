9c9e593722ec9763183cfb0997d914f21558b41f
mividalocas2-cloud <mi.vida.loca.s2@gmail.com>
2026-01-17 11:38:21 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from lineworks_auth import load_service_account
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"ok": True}
+
+@app.get("/debug/service-account")
+def debug_service_account():
+    data = load_service_account()
+    return {
+        "keys": list(data.keys())
+    }
